1a10d47fd9f386b89b58a9c7a9adf974f44b8e13
Oleg Shakurov <thorsonodin2003@gmail.com>
2025-05-15 17:25:50 +0500

    implement fix some styles

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,20 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+import clsx from "clsx";
+import { useTheme } from "next-themes";
 import React from "react";
 
 function Footer() {
+  const { theme } = useTheme();
   return (
-    <div className="flex items-center justify-center bg-[#0c0c0c] bottom-0 mt-8">
+    <div
+      className={clsx(
+        "flex items-center justify-center bottom-0 border-t-2",
+        theme === "dark"
+          ? "bg-[#0c0c0c]  border-white"
+          : "bg-white border-black"
+      )}
+    >
       <img src="/favicon.svg" alt="" />
     </div>
   );
